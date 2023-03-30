7322035198f5ec82d1f673c5cd80c4f2e5f88241
RomanKamko <r.kamko@mail.ru>
2023-03-30 21:37:50 +0400

    выполнил все задания

--- src/edu/skypro/homework/Main.java
+++ src/edu/skypro/homework/Main.java
@@ -4,10 +4,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         System.out.println(task1(nums));
         System.out.println(task2(nums));
+
         String s = "java, java, java, python, java, c#, java, python";
+        System.out.println(task3(s));
+        System.out.println(task4(s));
+
     }
 
     static String task1(List<Integer> integers) {
@@ -32,5 +37,37 @@ public class Main {
             }
         }
         return sb.toString();
+    }
+
+    // задание 3 и 4, не совсем понятно что хотят, сделал так как я это понял)))
+
+    static Set<String> task3(String s) {
+        String replace = s.replaceAll("\\p{Punct}", "");
+        List<String> stringList = List.of(replace.split(" "));
+        Set<String> stringSet = new HashSet<>();
+        for (int i = 0; i < stringList.size(); i++) {
+            String tmp = stringList.get(i);
+            if (Collections.frequency(stringList, tmp) > 1) {
+//                stringSet.add(tmp + " " + Collections.frequency(stringList, tmp));
+                stringSet.add(tmp);
+                // как я понял от меня хотят только вывод дублирующихся слов без их количества
+            }
+        }
+        return stringSet;
+    }
+
+    static Set<Integer> task4(String s) {
+        String replace = s.replaceAll("\\p{Punct}", "");
+        List<String> stringList = List.of(replace.split(" "));
+        Set<Integer> integerSet = new HashSet<>();
+        for (int i = 0; i < stringList.size(); i++) {
+            String tmp = stringList.get(i);
+            if (Collections.frequency(stringList, tmp) > 1) {
+                integerSet.add(Collections.frequency(stringList, tmp));
+                // как я понял от меня хотят только вывод сколько раз слово повторилось(если оно повторилось)
+                // и непонятно нужно просуммировать или вывести для каждого повтора свое число
+            }
+        }
+        return integerSet;
     }
 }
